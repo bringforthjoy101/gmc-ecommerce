fa710cbe61c96e991060e9e75bba5902c9ab78a3
bringforthjoy101 <bringforthjoy101@users.noreply.github.com>
2026-04-01 18:40:17 +0000

    fix: remove next param from bcrypt pre-save hook (Node 25 compat)

--- server/models/User.js
+++ server/models/User.js
@@ -25,10 +25,9 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving (only when modified)
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 // Instance method: compare plain-text password with stored hash
